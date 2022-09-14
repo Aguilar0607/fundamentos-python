1b54b039ae34a404a5cdbec337fa38ec5811bc00
Emmanuel <emmanuelaguilar0607@gmail.com>
2022-09-14 11:07:29 -0500

    años perrunos

---
+++ ageDog.py
@@ -0,0 +1,6 @@
+age_human = input("Escriba su edad: ")
+last_years = int(age_human) - 2
+first_years = 2 * 10.5
+last_years = 4 * last_years
+age_dog = first_years + last_years
+print(f"Tienes {age_human} años que equivalen a {age_dog} años perrunos") 
--- convertidor.py
+++ convertidor.py
@@ -1,5 +1,3 @@
 dolar = input("Ingrese el numero de dolares a convertir: ")
-
 pesos = float(dolar) * 20.90
-
 print(f"El resultado de la conversion es: {pesos} pesos")
